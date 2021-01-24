d3d24dfc9347a9741ab368a2b9b7fee0a3522b67
vkorostelev <vkorostelev@tsconsulting.com>
2021-01-24 21:04:44 +0300

    Second commit

--- src/main/resources/db/changelog/changes/001_Create_tables.sql
+++ src/main/resources/db/changelog/changes/001_Create_tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE public.boxes
     id int NOT NULL,
     contained_in int,
     CONSTRAINT boxes_pkey PRIMARY KEY (id),
-    CONSTRAINT fkg237sjg80fo9ufpa235ymn1l5 FOREIGN KEY (contained_in)
+    CONSTRAINT contained_in_fkey FOREIGN KEY (contained_in)
         REFERENCES public.boxes (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
@@ -13,19 +13,20 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.boxes
     OWNER to postgres;
-    CREATE TABLE public.items
+
+CREATE TABLE public.items
     (
         id int NOT NULL,
         color character varying(100) COLLATE pg_catalog."default",
         contained_in int,
         CONSTRAINT items_pkey PRIMARY KEY (id),
-        CONSTRAINT fkbm3athjb8ijoou9j1b7k15x0q FOREIGN KEY (contained_in)
+        CONSTRAINT contained_in_key FOREIGN KEY (contained_in)
             REFERENCES public.boxes (id) MATCH SIMPLE
             ON UPDATE NO ACTION
             ON DELETE NO ACTION
-    )
+)
 
-    TABLESPACE pg_default;
+TABLESPACE pg_default;
 
-    ALTER TABLE public.items
-        OWNER to postgres;
+ALTER TABLE public.items
+     OWNER to postgres;
